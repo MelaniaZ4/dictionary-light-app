208c6264f3a0fd10529e658b0eb790b73cc9d541
MelaniaZ4 <77643560+MelaniaZ4@users.noreply.github.com>
2021-05-06 19:38:46 +1200

    Changed App Name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <h1>Dictionary Light</h1>
+          <h1>Dictionary Lite</h1>
         </header>
         <main>
           <Dictionary />
